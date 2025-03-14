531b0156ee8b819519ce403453e2d0b12383ce5c
Netik Kohli <netikkohli9@gmail.com>
2025-03-14 08:50:00 +0530

    Load trained ML model into Flask API

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+import pickle
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Load trained model and vectorizer (pickled in Phase 1)
+model = pickle.load(open("best_model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Placeholder: prediction logic not yet implemented
-    return 'Not implemented', 501
+    data = request.get_json()
+    text = data.get('text', '')
+    # Transform and predict
+    vec = vectorizer.transform([text])
+    pred = model.predict(vec)[0]
+    # Return raw prediction (label) as JSON
+    return jsonify({ "prediction": int(pred) })
 
 if __name__ == '__main__':
     app.run(debug=True)
